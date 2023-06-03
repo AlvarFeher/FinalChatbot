4a8eca001bc5b66462d5647fa3d310dc76d05a97
Guillermo <guillermo.g@students.salle.url.edu>
2023-06-03 09:49:15 +0200

    Final version 1.0

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from nltk.stem.lancaster import LancasterStemmer
-import tensorflow as tf
+from telegram.constants import ParseMode
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Embedding, Flatten
 
-import json
 import numpy as np
 from telegram import *
 from telegram.ext import (
@@ -16,23 +15,65 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-import spacy
 import json
-import random
+import requests
 import tensorflow as tf
 from googleapiclient.discovery import build
 from typing import Final
 import csv
 import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment import SentimentIntensityAnalyzer
+from datetime import datetime
+from word2number import w2n
+
 stemmer = LancasterStemmer()
 
-TOKEN: Final = "6072166464:AAGK_3kc39vEAGwhNR2on90NZ95KXNNpQ-Y"  # bot token
-BOT_USERNAME: Final = '@MecagoensatanasBot'
+TOKEN: Final = "6054945876:AAFxijladNrhRgATM4iOwRXcMm_MfuEGBDQ"  # bot token
+BOT_USERNAME: Final = '@KBS23bot'
 YT_key: Final = 'AIzaSyBxNofXCt8LaoBJri2_lBhJGFWSMXM-oCE'
 
 qStarted: bool = False
 qIndex: int = 0  # test question index
+similarStarted: bool = False
+series = []
+finalList = []
+# Series age classification
+classifications = {
+    'A': 0,
+    'All': 0,
+    '7': 7,
+    '7+': 7,
+    'PG': 8,
+    '12+': 12,
+    'UA': 12,
+    '13': 13,
+    '13+': 13,
+    'U': 14,
+    '15': 15,
+    '15+': 15,
+    '16': 16,
+    '16+': 16,
+    '18': 18,
+    '18+': 18,
+    'nan': 18,
+    'R': 18,
+    'Not Rated': 18
+}
 
+currentSynonyms = ['current', 'present', 'contemporary', 'late', 'modern', 'new', 'actual', 'recent', 'fresh', 'trendy',
+                   'innovative']
+classicSynonyms = ['classic', 'classical', 'iconic', 'old', 'vintage', 'legendary', 'past', 'historic', 'historical',
+                   'previous', 'early', 'ancient', 'prior', 'old-fashioned']
+genres = ['action', 'adventure', 'drama', 'crime', 'thriller', 'horror', 'comedy', 'romance', 'fantasy', 'mystery',
+          'sci-fi', 'animation', 'biography', 'family', 'history', 'documentary', 'music', 'sport', 'western', 'war',
+          'musical', 'reality-tv', 'short', 'news', 'talk-show', 'game-show']
+relevantSynonyms = ['relevant', 'high', 'big', 'important', 'huge', 'elevated', 'giant', 'key', 'dominant', 'supreme',
+                    'crucial', 'vital', 'indispensable', 'critical', 'essential', 'significant', 'fundamental',
+                    'useful']
+irrelevantSynonyms = ['irrelevant', 'small', 'little', 'tiny', 'short', 'minor']
 
 # Load the data
 with open("data/intents.json") as file:
@@ -83,10 +124,11 @@ model.compile(loss="categorical_crossentropy",
 
 # Train the model
 model.fit(padded_sequences, one_hot_labels,
-          epochs=1000, batch_size=8, verbose=1)
+          epochs=500, batch_size=8, verbose=1)
 
 # Save the model
 model.save("model.h5")
+
 
 # predict to which category the input belongs to
 
@@ -104,48 +146,81 @@ def predict_tag(sentence):
         if intent["tag"] == predicted_label:
             return np.random.choice(intent["responses"])
 
+
 # Commands
 
 
 async def startCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hi! I'm here to recommend TV Series. Talk to me and discover what I have to offer ;)")
+    await update.message.reply_text(
+        "Hi! I'm here to recommend TV Series. Talk to me and discover what I have to offer ;)")
 
 
 async def helpCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Need help huh?\nYou can just talk to me and i'll try my best to answer something that makes sense.\nIf you ask for a questionaire i'll eventually recommend something\n(I can also give you trailers from tv series, that's cool right?)")
+    await update.message.reply_text(
+        "Need help huh?\nYou can just talk to me and i'll try my best to answer something that makes sense.\nIf you ask for a questionaire i'll eventually recommend something\n(I can also give you trailers from tv series, that's cool right?)")
 
 
 async def stopCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("cya buddy")
     context.bot.stop()
 
+
+async def getSimilarShows(text: str):
+    global similarStarted
+
+    API_KEY = '4ec06d8570075916af7293379cbf5430'
+
+    # Search for the TV show you want to find similar shows to
+    url = f"https://api.themoviedb.org/3/search/tv?api_key={API_KEY}&query={text}"
+    response = requests.get(url)
+    results = response.json()["results"]
+
+    # Get the ID of the first search result
+    tv_id = results[0]["id"]
+
+    # Get the similar TV shows
+    url = f"https://api.themoviedb.org/3/tv/{tv_id}/similar?api_key={API_KEY}"
+    response = requests.get(url)
+    similar_shows = response.json()["results"]
+
+    # Print the titles of the three most similar shows
+    for show in similar_shows[:3]:
+        print(show["name"])
+    similarStarted = False
+
+    return similar_shows
+
+
 # async def resultCommand():
 
 # get all movies from the dataset
 
 
-def getAllMovies():
-    movies = []
+def getAllSeries():
     with open('data/series_data.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
-            movie = {
-                'episodes': row[0],
-                'genre': row[1],
-                'imdb_rating': row[2],
-                'overview': row[3],
-                'star1': row[4],
-                'star2': row[5],
-                'star3': row[6],
-                'star4': row[7],
-                'no_of_votes': row[8]
+            serie = {
+                'poster': row[0],
+                'title': row[1],
+                'period': row[2],
+                'certificate': row[3],
+                'duration': getDuration(row[4]),
+                'genre': row[5],
+                'imdb_rating': row[6],
+                'overview': row[7],
+                'star1': row[8],
+                'star2': row[9],
+                'star3': row[10],
+                'star4': row[11],
+                'no_of_votes': row[12]
             }
-            movies.append(movie)
-            # print(movie)
-        # Append the movie to the list
+            # Append the serie to the list
+            series.append(serie)
 
-    return movies
+    return series
+
 
 # Responses
 
@@ -165,15 +240,55 @@ async def handleMessage(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text: str = update.message.text  # user input
     global qStarted
     global qIndex
-    questions = ["q1", "q2", "q3"]
+    global similarStarted
+    questions = ["How old are you?", "How long are the episodes of the series you usually watch? (in minutes)",
+                 "Do you prefer current or classic series?", "What genres are you looking for? \U0001F3AD",
+                 "How relevant is the opinion of critics to you? \U0001F4DD\U0001F440"]
     if text == "/questions":
         qStarted = True
-        await update.message.reply_text("Starting questionnaire...")
-    elif qStarted:
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text='I will ask you a few questions to know more about you \U0001F50E\nStarting questionnaire...')
         await update.message.reply_text(questions[qIndex])
+    elif text == "/similar":
+        similarStarted = True
+        await update.message.reply_text("Give me the name of a tv-show please: ")
+    elif similarStarted:
+        print("similar ongoing")
+        similarShows = await getSimilarShows(text)
+        for show in similarShows[:3]:
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text="*" + show["name"] + "* \u2935\uFE0F",
+                                           parse_mode=ParseMode.MARKDOWN)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="https://image.tmdb.org/t/p/w500" +
+                                                                                  show["poster_path"])
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=show["overview"])
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text=search_video("Trailer of " + show["name"], YT_key))
+    elif qStarted:
         answer: str = await handleQuestion(text)
-        await update.message.reply_text(answer)
-        qIndex = qIndex + 1
+        result = processResponse(answer, qIndex)
+        if result is not None:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+        else:
+            qIndex = qIndex + 1
+        if qIndex < 5:
+            await update.message.reply_text(questions[qIndex])
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text="That's the end of the questionnaire.\nYou got a \U0001F4AF\nHere are my recommendations \U0001F60F")
+
+            finalSortedList = sorted(finalList, key=lambda x: x['imdb_rating'], reverse=True)
+            for i in range(0, 3, 1):
+                await context.bot.send_message(chat_id=update.effective_chat.id,
+                                               text="*" + finalSortedList[i]['title'] + "* \u2935\uFE0F",
+                                               parse_mode=ParseMode.MARKDOWN)
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=finalSortedList[i]['poster'])
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=finalSortedList[i]['overview'])
+                await context.bot.send_message(chat_id=update.effective_chat.id,
+                                               text=search_video("Trailer of " + finalSortedList[i]['title'], YT_key))
+            qStarted = False
+            qIndex = 0
+
     else:
         response: str = await handleResponse(text)
         print('Bot:', response)
@@ -187,6 +302,194 @@ async def handleQuestion(input: str):
     global qIndex
 
     return input
+
+
+# Process questionnaire responses
+def getNum(word):
+    try:
+        num = w2n.word_to_num(word)
+        return num
+    except ValueError:
+        return None
+
+
+def isYear(year):
+    try:
+        datetime.strptime(str(year), "%Y")
+        return True
+    except ValueError:
+        return False
+
+
+def getCertificate(response):
+    tokens = nltk.word_tokenize(response)
+    age = None
+    for token in tokens:
+        age = getNum(token)
+        if age is not None:
+            break
+
+    if isYear(age):
+        age = datetime.now().year - age
+
+    if age is not None:
+        eligibleClass = []
+        for classification, appropAge in classifications.items():
+            if age >= appropAge:
+                eligibleClass.append(classification)
+        return eligibleClass
+    else:
+        return age
+
+
+def getDuration(response):
+    tokens = nltk.word_tokenize(response)
+    for token in tokens:
+        time = getNum(token)
+        if time is not None:
+            return time
+
+    return None
+
+
+def determinePreference(response):
+    tokens = word_tokenize(response.lower())
+    stopWords = set(stopwords.words('english'))
+    filteredTokens = [token for token in tokens if token not in stopWords]
+    lemmatizer = WordNetLemmatizer()
+    lemmatizedTokens = [lemmatizer.lemmatize(token, pos='a') for token in filteredTokens]
+
+    for token in lemmatizedTokens:
+        if token in currentSynonyms:
+            return "current"
+        elif token in classicSynonyms:
+            return "classic"
+
+    return None
+
+
+def isPositive(response):
+    sia = SentimentIntensityAnalyzer()
+    sentiment = sia.polarity_scores(response)
+    if sentiment['compound'] >= 0:
+        return True
+    else:
+        return False
+
+
+def checkFinalListDecade(preference):
+    for serie in reversed(finalList):
+        if (preference == "current" and 2000 > int(serie['period'].split('–')[0].strip('()'))) or \
+                (preference == "classic" and 2000 <= int(serie['period'].split('–')[0].strip('()'))):
+            finalList.remove(serie)
+
+
+def getGenres(response):
+    genreList = []
+    tokens = word_tokenize(response.lower())
+    stopWords = set(stopwords.words('english'))
+    filteredTokens = [token for token in tokens if token not in stopWords]
+
+    if isPositive(response):
+        for token in filteredTokens:
+            if token in genres:
+                genreList.append(token)
+    else:
+        genreList = genres
+        for token in filteredTokens:
+            if token in genreList:
+                genreList.remove(token)
+
+    return genreList if genreList else None
+
+
+def filterByGenre(genreList):
+    match = False
+    for serie in reversed(finalList):
+        serieGenres = serie['genre'].split(", ")
+        serieGenres = [genre.lower() for genre in serieGenres]
+
+        for genre in serieGenres:
+            if genre in genreList:
+                match = True
+
+        if not match:
+            finalList.remove(serie)
+        else:
+            match = False
+
+
+def isRelevant(response):
+    if isPositive(response):
+        tokens = word_tokenize(response.lower())
+        stopWords = set(stopwords.words('english'))
+        filteredTokens = [token for token in tokens if token not in stopWords]
+        lemmatizer = WordNetLemmatizer()
+        lemmatizedTokens = [lemmatizer.lemmatize(token, pos='a') for token in filteredTokens]
+
+        for token in lemmatizedTokens:
+            if token in relevantSynonyms:
+                return True
+            elif token in irrelevantSynonyms:
+                return False
+
+        return None
+    else:
+        return False
+
+
+def filterByRating():
+    for serie in finalList:
+        if float(serie['imdb_rating']) < 7.5:
+            finalList.remove(serie)
+
+
+def processResponse(response: str, question: int):
+    if question == 0:
+        userCertificate = getCertificate(response)
+        if userCertificate is None:
+            return "From the answer given I cannot determine your age"
+        else:
+            # Get series having that certificate
+            finalList.clear()
+            for serie in series:
+                if serie['certificate'] in userCertificate:
+                    finalList.append(serie)
+            return None
+    elif question == 1:
+        userDuration = getDuration(response)
+        if userDuration is None:
+            return "Tell me a specific duration value"
+        else:
+            for serie in reversed(finalList):
+                if serie['duration'] is None or abs(userDuration - serie['duration']) > 10:
+                    finalList.remove(serie)
+            return None
+    elif question == 2:
+        userPreference = determinePreference(response)
+        if userPreference is None:
+            return "This is not what I am asking \U0001F914"
+        else:
+            if not isPositive(response):
+                userPreference = "classic" if userPreference == "current" else "current"
+            checkFinalListDecade(userPreference)
+            return None
+    elif question == 3:
+        userGenres = getGenres(response)
+        if userGenres is None:
+            return "I don't have this genre on my list \U0001F615"
+        else:
+            filterByGenre(userGenres)
+            return None
+    elif question == 4:
+        criticsRelevance = isRelevant(response)
+        if criticsRelevance is None:
+            return "I don't get what you are trying to say \U0001F62C\U0001F937"
+        else:
+            if criticsRelevance is True:
+                filterByRating()
+            return None
+
 
 # when the user inputs the command /questions, start the questionaire
 
@@ -234,15 +537,17 @@ def search_video(title, api_key):
 
     return video_link
 
+
 # Errors
 
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'error caused by {update}: {context.error}')
 
+
 # Main
 if __name__ == '__main__':
-    movies = getAllMovies()
+    getAllSeries()
     print('Starting bot...')
     app = Application.builder().token(TOKEN).build()
 
